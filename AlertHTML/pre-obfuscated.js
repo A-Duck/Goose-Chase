a12ea89d37ca01dda4f9fefcc6ef06da5d3d1332
A-Duck <A-Duck@users.noreply.github.com>
2022-03-07 23:18:47 +0200

    Add Website

---
+++ AlertHTML/pre-obfuscated.js
@@ -0,0 +1,214 @@
+function fun() {
+    alert("Let's set out some ground rules...");
+    alert("Rule 1:\nDon't click that silly `Don't let the website prompt me anymore` button\n(If you see one)");
+    alert("I'm going to give you some important information, you need to remember it");
+    alert("I'll be nice and let you use a pen & paper");
+    alert("Rule 2:\nI talk, you acknowledge\nHopefully we get through this quickly");
+    alert("** Snickers **");
+    alert("Because you're not you when youre hungry");
+    alert("And dont get any cute ideas like checking the source code");
+    alert("You cant access this section");
+    alert("I checked");
+    alert("Mwahahahaha");
+    alert("Oh also...");
+    alert("One more thing...");
+    alert("I might get distracted");
+    pointlessquestion("so... Understand the rules?");
+    alert("Fantastic!");
+    alert("Let's Begin");
+    alert("also don't all these messages remind you of that episode in VGHS?");
+    alert("The one where Brian and Ted are trying to get Babages blade...");
+    alert("You've seen it right?");
+    alert("It's S03e01");
+    alert("It was a great show");
+    alert("Let's watch it...\nIn case you don't have it - https://www.youtube.com/watch?v=nhiRE2hfFJc");
+    alert("So good...");
+    alert("Question 1:\nProve you are worthy...");
+    notpointlessquestion("By Entering the exclusive pre-order bonus code", "3BBXXYl", "You are... not worthy!", "You are worthy!");
+    alert("Enjoy your adventure");
+    alert("but before you do...");
+    alert("Seeing the similarities yet?");
+    alert("anyways...");
+    alert("I got you to watch an episode of VGHS\ni should probably give you a clue now");
+    alert("or we could watch something else?");
+    alert("No wait...");
+    alert("I have a better ideea");
+    alert("Karaoke!");
+    alert("But you don't really know my music...");
+    alert("What's something you'd know...");
+    alert("** Scrolls through library **");
+    alert("Here's one...");
+    alert("You should know it");
+    alert("and the lyrics should be easy to follow");
+    alert("It's xalled Around the world");
+    alert("Ready?");
+    alert("** Presses Play **");
+    alert("(Intro)");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    alert("Around the world, around the world");
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    noresponsequestion('', 'Around the world, around the world', 'Oops; Try again.\nI\'ll let you google the lyrics if it helps');
+    alert("** Song ends **");
+    alert("That was fun!");
+    alert("I'll let you get back to your quest");
+    alert("I think you've earned your clue");
+    alert("It is...");
+    alert("** Drumroll **");
+    alert("** Still rolling... **");
+    alert("** Rollin'... **");
+    alert("** in my 5.0 **");
+    alert("** With the ragtop down so my hair can blow **");
+    alert("** The girlies on standby, wavin' just to say hi **");
+    alert("** Did you stop? No, I just drove by **");
+    alert("** Kept on, pursuin' to the next stop **");
+    alert("** I busted a left and I'm headin' to the next stop **");
+    alert("** The block was dead, yo, so I continued to **");
+    alert("** A1A Beachfront Avenue **");
+    alert("** Girls were hot wearing less than bikinis **");
+    alert("** Rockman lovers driving Lamborghinis **");
+    alert("** Jealous, 'cause I'm out getting mine **");
+    alert("** Shay with a gauge and Vanilla with a 9 **");
+    alert("** Ready for the chumps on the wall **");
+    alert("** The chumps acting ill because they're full of eight ball **");
+    alert("** Gunshots raged out like a bell **");
+    alert("** I grabbed my 9, all I heard were shells **");
+    alert("** Falling on the concrete real fast **");
+    alert("** Jumped in my car, slammed on the gas **");
+    alert("** Bumper to bumper, the avenue's packed **");
+    alert("** I'm trying to get away before the jackers jack **");
+    alert("** Police on the scene, you know what I mean? **");
+    alert("** They passed me up, confronted all the dope fiends **");
+    alert("** If there was a problem, yo, I'll solve it **");
+    alert("** Check out the hook while my DJ revolves it **");
+    alert("** Ice, ice, baby **");
+    alert("** Vanilla Ice, ice, baby **");
+    alert("** Vanilla Ice, ice, baby **");
+    alert("** Ice, ice, baby **");
+    alert("**  **");
+    alert("** Fun fact: I actually do know the lyrics to this song **");
+    alert("** Like all of it **");
+    alert("** No cap **");
+    alert("** Anyways, where were we? **");
+    alert("** Oh right... **");
+    alert("** Drumroll ends  **");
+    alert("** Now we pause for tension **");
+    alert("** ... **");
+    alert("** ... **");
+    alert("** ... **");
+    alert("** ... **");
+    alert("** ... **");
+    alert("** ... **");
+    alert("** ... **");
+    alert("** ... **");
+    redirect();
+}
+
+// -----------------------------------------------------------------------------------------
+
+function nofun() {
+    alert("To bad! We're doing it anyway...");
+    fun()
+}
+
+// -----------------------------------------------------------------------------------------
+
+function pointlessquestion(msg) {
+    prompt(msg, "");
+}
+
+// -----------------------------------------------------------------------------------------
+
+function redirect()
+{
+    window.location.href = "finale.html";
+}
+
+// -----------------------------------------------------------------------------------------
+
+function notpointlessquestion(msg, answer, FailMsg, SuccessMsg) {
+    let response = prompt(msg, "");
+    if (answer.toLowerCase() !== response.toLowerCase()) {
+        alert(FailMsg)
+        notpointlessquestion(msg, answer, FailMsg, SuccessMsg)
+    } else {
+        alert(SuccessMsg)
+    }
+}
+
+// -----------------------------------------------------------------------------------------
+
+function noresponsequestion(msg, answer, FailMsg) {
+    let response = prompt(msg, "");
+    if (answer.toLowerCase() !== response.toLowerCase()) {
+        alert(FailMsg)
+        noresponsequestion(msg, answer, FailMsg)
+    }
+}
